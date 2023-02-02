0b4ffcb49365e5022f405bb8f9bec072fc6634a4
shn <topdog.shn.01@gmail.com>
2023-02-02 22:47:32 +0900

    ✨ ページングを実装

--- src/components/BlogSearch.tsx
+++ src/components/BlogSearch.tsx
@@ -1,16 +1,23 @@
 import useSWR from "swr";
+import { useState } from "preact/hooks";
 import { getBlogs } from "../library/microcms";
+
+const LIMIT = 1; //ページングテスト用に1件表示
 
 const BlogSearch = () => {
 	const params = new URLSearchParams(window.location.search);
 	const q = params.get("q");
 
+	const [page, setPage] = useState(1);
+
 	const { data, error, isLoading } = useSWR(
-		q === null ? null : ["/search", q],
-		([, q]) =>
+		q === null ? null : ["/search", q, page],
+		([, q, page]) =>
 			getBlogs({
 				fields: ["id", "title"],
 				q,
+				limit: LIMIT,
+				offset: (page - 1) * LIMIT,
 			})
 	);
 
@@ -21,13 +28,33 @@ const BlogSearch = () => {
 	return (
 		<div>
 			{data?.contents.length !== 0 ? (
-				<ul>
-					{data?.contents.map(({ id, title }) => (
-						<li key={id}>
-							<a href={id}>{title}</a>
-						</li>
-					))}
-				</ul>
+				<>
+					<ul>
+						{data?.contents.map(({ id, title }) => (
+							<li key={id}>
+								<a href={id}>{title}</a>
+							</li>
+						))}
+					</ul>
+					{data?.totalCount !== undefined && (
+						<nav>
+							<ul style={{ display: "flex", gap: "8px" }}>
+								{Array.from({
+									length: Math.ceil(data.totalCount / LIMIT),
+								}).map((_, i) => (
+									<li key={i} style={{ listStyle: "none" }}>
+										<button
+											onClick={() => setPage(i + 1)}
+											disabled={page === i + 1}
+										>
+											{i + 1}
+										</button>
+									</li>
+								))}
+							</ul>
+						</nav>
+					)}
+				</>
 			) : (
 				<div>検索結果はありません</div>
 			)}
